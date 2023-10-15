2e2785df7a65569cf1d36e708b1ae9d2b8c9d0b9
Adrian <56790744+zoladrian@users.noreply.github.com>
2023-10-16 00:07:59 +0200

    Initial commit

---
+++ FactoryAPI/Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FactoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        // Create a new item
+
+
+        // Retrieve a single item
+
+
+        // Retrieve all items (only if you'll be using an ORM framework)
+
+
+        // Update an existing item
+
+
+        // Delete an item
+
+
+    }
+}
