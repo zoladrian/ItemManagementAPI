9886215abe7e862b2cd283a6ac8e53ffe448a7bd
Adrian <56790744+zoladrian@users.noreply.github.com>
2023-10-17 14:11:24 +0200

    update itemscontroller

--- FactoryAPI/Controllers/ItemsController.cs
+++ FactoryAPI/Controllers/ItemsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FactoryAPI.DbContexts;
+using FactoryAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FactoryAPI.Controllers
 {
@@ -7,20 +9,68 @@ namespace FactoryAPI.Controllers
     [ApiController]
     public class ItemsController : ControllerBase
     {
+        private readonly ItemDbContext _context;
+
+        public ItemsController(ItemDbContext context)
+        {
+            _context = context;
+        }
+
         // Create a new item
+        [HttpPost]
+        public async Task<IActionResult> CreateItem(Item item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetItemById), new { id = item.ID }, item);
+        }
 
+        // Retrieve a single item by ID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItemById(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
 
-        // Retrieve a single item
-
-
-        // Retrieve all items (only if you'll be using an ORM framework)
-
+        // Retrieve all items
+        [HttpGet]
+        public async Task<IActionResult> GetAllItems()
+        {
+            return Ok(await _context.Items.ToListAsync());
+        }
 
         // Update an existing item
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, Item item)
+        {
+            if (id != item.ID)
+            {
+                return BadRequest();
+            }
 
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
 
         // Delete an item
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
 
-
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
