b32ad21d60a8c89a65f3d294e375b7b460540de3
Adrian <56790744+zoladrian@users.noreply.github.com>
2023-10-17 14:12:19 +0200

    Add validation

--- FactoryAPI/Models/Item.cs
+++ FactoryAPI/Models/Item.cs
@@ -1,11 +1,19 @@
-﻿namespace FactoryAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FactoryAPI.Models
 {
     public class Item
     {
         public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [MaxLength(250)]
         public string Description { get; set; }
+
+        [Range(0, 9999.99)]
         public decimal Price { get; set; }
     }
-
 }
