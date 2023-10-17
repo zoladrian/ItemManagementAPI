3706f448c65b02ff5bf96b122f10db8aedeacfbd
Adrian <56790744+zoladrian@users.noreply.github.com>
2023-10-17 14:08:10 +0200

    Add item model and itemDbContext

---
+++ FactoryAPI/DbContexts/ItemDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FactoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FactoryAPI.DbContexts
+{
+    public class ItemDbContext : DbContext
+    {
+        public ItemDbContext(DbContextOptions<ItemDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+
+}
---
+++ FactoryAPI/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoryAPI.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
